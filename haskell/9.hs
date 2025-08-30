16122e73ff3a345adc8d36e4f195497718074a06
codingp110 <codingp110@gmail.com>
2025-08-30 23:17:49 +0530

    feat: add solution for 9 in Haskell

---
+++ haskell/9.hs
@@ -0,0 +1,10 @@
+inAcc x xs = null xs || (x == head xs)
+groupList xs = groupList' xs [] [] 
+    where
+        groupList' [x] acc list =  if inAcc x acc then list++[acc++[x]] else (list++[acc])++[[x]]
+        groupList' (x:xs) acc list =
+            if  inAcc x acc
+            then 
+                groupList' xs (acc++[x]) list 
+            else 
+                groupList' xs [x] (list++[acc])
